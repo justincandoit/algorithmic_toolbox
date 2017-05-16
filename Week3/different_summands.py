4a19274259738f958df49e52b5415dbb83911429
Justin <justinc97@gmail.com>
2017-05-15 21:47:48 -0400

    two more files

--- Week3/different_summands.py
+++ Week3/different_summands.py
@@ -3,7 +3,21 @@ import sys
 
 def optimal_summands(n):
     summands = []
-    #write your code here
+
+    if n <= 2:
+        summands.append(n)
+
+    else:
+        l = 1
+        while (n > 2 * l):
+            summands.append(l)
+            n -= l
+            l += 1
+        summands.append(n)
+
+
+
+
     return summands
 
 if __name__ == '__main__':
