5c95a6d5764f7944920a54b57ae7e6b7f25e82d8
Justin <justinc97@gmail.com>
2017-05-16 16:39:36 -0400

    greatest number solution

--- Week3/largest_number.py
+++ Week3/largest_number.py
@@ -2,16 +2,39 @@
 
 import sys
 
+def firstdigit(elem):
+    return elem[0]
+
+def isgore(a, maxb):
+    a = str(a) + str(maxb)
+    maxb = str(maxb) + str(a)
+    if a > maxb:
+        return True
+    else:
+        return False
+
+
+
+
 def largest_number(a):
-    #write your code here
+    answer = []
+    while a != []:
+        maxdigit = int('-99999')
+        for digit in a:
+            digit = int(digit)
+            if isgore(digit, int(maxdigit)):
+                maxdigit = digit
+        answer.append(maxdigit)
+        a.remove(maxdigit)
+
+
     res = ""
-    for x in a:
-        res += x
+    for x in answer:
+        res += str(x)
     return res
 
 if __name__ == '__main__':
     input = sys.stdin.read()
-    data = input.split()
+    data = list(map(int, input.split()))
     a = data[1:]
     print(largest_number(a))
-    
